97069837236728de049e9b50805b1bbb86279458
Gabriel Roland Carvalho Fonseca <GRCFO@bndes.net>
2019-11-27 10:45:31 -0300

    Criado repositorio e adicionados os arquivos

---
+++ aluratest/src/app/app.routing.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { PhotosListComponent } from './photos/photos-list/photos-list.component';
+import { PhotosFormComponent } from './photos/photos-form/photos-form.component';
+import { NotFoundComponent } from './errors/not-found/not-found.component';
+
+const routes: Routes = [
+   {path: 'user/:userName', component: PhotosListComponent},
+   {path: 'p/add', component: PhotosFormComponent},
+   {path: '**', component: NotFoundComponent}
+
+];
+
+@NgModule({
+    imports: [ RouterModule.forRoot(routes) ],
+    exports: [ RouterModule ]
+})
+
+export class AppRoutingModule{
+
+}
