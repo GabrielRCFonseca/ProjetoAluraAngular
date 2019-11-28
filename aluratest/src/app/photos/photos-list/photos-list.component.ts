9348dcccc4689493ad5ec437a3dc7ba7181ac7e0
Gabriel Roland Carvalho Fonseca <GRCFO@bndes.net>
2019-11-28 11:09:16 -0300

    Adicionado ngOnDestroy e minor changes

--- aluratest/src/app/photos/photos-list/photos-list.component.ts
+++ aluratest/src/app/photos/photos-list/photos-list.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { debounceTime } from 'rxjs/operators';
+ 
 import { Photo } from '../photo/photo';
-import { Subject } from 'rxjs';
-import { filter } from 'minimatch';
+import { Subject, from } from 'rxjs';
 
 @Component({
   selector: 'ap-photos-list',
   templateUrl: './photos-list.component.html',
   styleUrls: ['./photos-list.component.css']
 })
-export class PhotosListComponent implements OnInit {
+export class PhotosListComponent implements OnInit, OnDestroy {
   
   photos: Photo[] = [];
   filter: string = ' ';
@@ -18,7 +19,15 @@ export class PhotosListComponent implements OnInit {
   constructor(private activatedRoute: ActivatedRoute){ }
 
   ngOnInit(): void {
-  this.photos = this.activatedRoute.snapshot.data['photos'];
-  this.debounce.subscribe(filter => this.filter = filter);
+    this.photos = this.activatedRoute.snapshot.data['photos'];
+    this.debounce
+      .pipe(debounceTime(300))
+      .subscribe(filter => this.filter = filter);
   }
+
+  ngOnDestroy():void{
+    this.debounce.unsubscribe();
+
+  }
+
 }
