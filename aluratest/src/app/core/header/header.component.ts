e4a88723e65d7f05f2beb0f7868b2e98fa6de852
Gabriel Roland Carvalho Fonseca <GRCFO@bndes.net>
2019-12-11 12:27:25 -0300

    minor changes

--- aluratest/src/app/core/header/header.component.ts
+++ aluratest/src/app/core/header/header.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { UserSevice } from '../user/user.service';
+import { Observable } from 'rxjs';
+import { User } from '../user/user';
 
 @Component({
     selector: 'ap-header',
@@ -6,5 +9,13 @@ import { Component } from '@angular/core';
 })
 
 export class HeaderComponent{
+
+    user$: Observable<User>;
+    user: User;
+
+    constructor(private userService: UserSevice){
+        this.user$ = userService.getUser();
+        this.user$.subscribe(user => this.user = user);
+    }
 
 }
